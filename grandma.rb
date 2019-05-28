bfb040605793eee6fb84067c13c46d19caebf221
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-28 16:24:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,11 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+def speak_to_grandman(greeting)
+  if greeting == "Hey Grandma!"
+    puts "Hello!"
+  puts "HUH?! SPEAK UP, SONNY!"
+end
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
@@ -11,3 +15,5 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+speak_to_grandman("Hey Grandma!")
