11bab0346c5306ed21693b9bf34987b93fa80674
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-28 16:28:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandman(greeting)
   if greeting == greeting.upcase
     puts "NO, NOT SINCE 1938!"
   elsif greeting == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
